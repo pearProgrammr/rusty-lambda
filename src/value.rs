f7db6011c769aa428242bea1267e86688523cfe3
D Scott Phillips <dsp2@pdx.edu>
2018-08-13 14:11:26 -0700

    eval, type_check: Add closures
    Change the 'lambda' values to 'closure' values by also holding a
    captured environment.

--- src/value.rs
+++ src/value.rs
@@ -1,9 +1,11 @@
 use ast::*;
+use std::collections::HashMap;
+
 #[derive(PartialEq, Debug, Clone)]
 /// This enum represents all possible values that a term can evaluate to.
 pub enum Value {
     Num(u64),
     Bool(bool),
-    Func { name: String, func_term: Box<Term> },
+    Closure { env: HashMap<String, Value>, name: String, func_term: Box<Term> },
     Assignm { name: String, val: Box<Value> },
 }
